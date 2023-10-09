ae07d7b7bcdd3b0d3c90b854bed7dc2bba4483f7
Niccolo <niccolo.ciommo2015@gmail.com>
2023-10-09 12:20:45 +0200

    commit pulita

--- src/main/java/com/example/demo/orari/OrariRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.orari;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-
-public interface OrariRepository extends JpaRepository<Orari, Long> {
-}
